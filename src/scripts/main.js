655afa414b5737167a54a062ecd20773568360be
Anton Donchenko <donch.a080@gmail.com>
2025-03-22 19:03:24 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const largeImg = document.querySelector('.gallery__large-img');
+
+function handleClick() {
+  event.preventDefault();
+
+  const img = this.querySelector('img');
+
+  if (img) {
+    largeImg.src = this.href;
+    largeImg.alt = img.alt;
+  }
+}
+
+const linkList = document.querySelectorAll('a.list-item__link');
+
+linkList.forEach((link) => {
+  link.addEventListener('click', handleClick);
+});
